d29a86ac331a3f50610169ba3910f42e9acc06af
JuanDavidSabogal <juan.1999.david@gmail.com>
2021-10-26 11:09:26 -0500

    Se agrego un nuevo Avion y Nuevo Aeropuerto

--- semana2_avion.sql
+++ semana2_avion.sql
@@ -1,53 +1,9 @@
--- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
---
--- Host: localhost    Database: semana2
--- ------------------------------------------------------
--- Server version	8.0.27
+SELECT * FROM semana2.avion;
+use avion;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `avion`
---
-
-DROP TABLE IF EXISTS `avion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `avion` (
-  `modeloId` varchar(10) NOT NULL,
-  `pasajeros` int NOT NULL,
-  `destino` varchar(10) NOT NULL,
-  `horas` int NOT NULL,
-  PRIMARY KEY (`modeloId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `avion`
---
-
-LOCK TABLES `avion` WRITE;
-/*!40000 ALTER TABLE `avion` DISABLE KEYS */;
-INSERT INTO `avion` VALUES ('DFA-25',40,'Japon',5),('JASDF-21',156,'Rusia',9),('JDSA-11',150,'China',4),('KMA-50',175,'Corea',12),('XJS\"-5',250,'Alemania',10);
-/*!40000 ALTER TABLE `avion` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-10-25 17:08:17
+INSERT INTO avion VALUES ('JDSA-11',150,'China',4);
+INSERT INTO avion VALUES ('XJS"-5',250,'Alemania',10);
+INSERT INTO avion VALUES ('DFA-25',40, 'Japon',5);
+INSERT INTO avion VALUES ('KMA-50',175,'Corea', 12);
+INSERT INTO avion VALUES ('JASDF-21',156,'Rusia',9);
+INSERT INTO avion VALUES ('XJ01-01',200,'EEUU',10);
