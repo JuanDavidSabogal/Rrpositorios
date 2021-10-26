1f5335312d0a353f7ba13c2de2b99b9110dc815b
JuanDavidSabogal <juan.1999.david@gmail.com>
2021-10-26 12:55:12 -0500

    Pasamos a la rama QA

--- semana2_aeropuerto.sql
+++ semana2_aeropuerto.sql
@@ -1,3 +1,7 @@
 SELECT * FROM semana2.aeropuerto;
 
-INSERT INTO aeropuerto (codigoId,Ruta,aviones) VALUES (7,'CH','XJ02-02')
+INSERT INTO aeropuerto (codigoId,Ruta,aviones) VALUES (7,'CH','XJ02-02');
+INSERT INTO aeropuerto VALUES(8,'JP','XJ03-03');
+INSERT INTO aeropuerto VALUES(9,'CR','XJ04-04');
+INSERT INTO aeropuerto VALUES(10,'FR','XJ05-05');
+INSERT INTO aeropuerto VALUES(11,'IT','XJ06-06');
--- semana2_avion.sql
+++ semana2_avion.sql
@@ -1,10 +1,14 @@
 SELECT * FROM semana2.avion;
 use avion;
 
-INSERT INTO avion VALUES ('JDSA-11',150,'China',4);
-INSERT INTO avion VALUES ('XJS"-5',250,'Alemania',10);
-INSERT INTO avion VALUES ('DFA-25',40, 'Japon',5);
-INSERT INTO avion VALUES ('KMA-50',175,'Corea', 12);
-INSERT INTO avion VALUES ('JASDF-21',156,'Rusia',9);
-INSERT INTO avion VALUES ('XJ01-01',200,'EEUU',10);
-INSERT INTO avion VALUES ('XJ02-02',200,'Canada',8);
+INSERT INTO avion VALUES ('JDSA-11' , 150 , 'China', 4);
+INSERT INTO avion VALUES ('XJS"-5' , 250 , 'Alemania' , 10);
+INSERT INTO avion VALUES ('DFA-25' , 40 , 'Japon' , 5);
+INSERT INTO avion VALUES ('KMA-50' , 175 , 'Corea', 12);
+INSERT INTO avion VALUES ('JASDF-21' , 156 , 'Rusia' , 9 );
+INSERT INTO avion VALUES ('XJ01-01' , 200 , 'EEUU', 10);
+INSERT INTO avion VALUES ('XJ02-02' , 200 , 'Canada', 8);
+INSERT INTO avion VALUES ('XJ03-03' , 250 , 'Japon' , 7 ); 
+INSERT INTO avion VALUES ('XJ04-04' , 175 , 'Colombia',10);
+INSERT INTO avion VALUES ('XJ05-05' , 160 , 'Rusia' , 15);
+INSERT INTO avion VALUES ('XJ06-06' , 158 , 'Francia' , 9);
