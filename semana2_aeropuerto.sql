5fe10a3361893718a021b60b17c345e426a4fa67
JuanDavidSabogal <juan.1999.david@gmail.com>
2021-10-26 13:12:45 -0500

    Errores de localizacion

--- semana2_aeropuerto.sql
+++ semana2_aeropuerto.sql
@@ -5,3 +5,4 @@ INSERT INTO aeropuerto VALUES(8,'JP','XJ03-03');
 INSERT INTO aeropuerto VALUES(9,'CR','XJ04-04');
 INSERT INTO aeropuerto VALUES(10,'FR','XJ05-05');
 INSERT INTO aeropuerto VALUES(11,'IT','XJ06-06');
+INSERT INTO aeropuerto VALUES(12,'MX','XJ07-07');
--- semana2_avion.sql
+++ semana2_avion.sql
@@ -12,3 +12,4 @@ INSERT INTO avion VALUES ('XJ03-03' , 250 , 'Japon' , 7 );
 INSERT INTO avion VALUES ('XJ04-04' , 175 , 'Colombia',10);
 INSERT INTO avion VALUES ('XJ05-05' , 160 , 'Rusia' , 15);
 INSERT INTO avion VALUES ('XJ06-06' , 158 , 'Francia' , 9);
+INSERT INTO avion VALUES ('XJ07-07' , 180 , 'Italia' , 9);
