d9520e6bcd3855a5ae97b007865a1e7fbbf5a1ef
JuanDavidSabogal <juan.1999.david@gmail.com>
2021-10-30 09:13:22 -0500

    Se modifico una linea de la DB para armar un conglito

--- semana2_aeropuerto.sql
+++ semana2_aeropuerto.sql
@@ -6,3 +6,8 @@ INSERT INTO aeropuerto VALUES(9,'CR','XJ04-04');
 INSERT INTO aeropuerto VALUES(10,'FR','XJ05-05');
 INSERT INTO aeropuerto VALUES(11,'IT','XJ06-06');
 INSERT INTO aeropuerto VALUES(12,'MX','XJ07-07');
+INSERT INTO aeropuerto VALUES (13,'VN','XJ08-08');
+INSERT INTO aeropuerto VALUES (14,'FN','XJ09-09');
+
+UPDATE aeropuerto SET Ruta ='DR' where  codigoId = 1;
+
